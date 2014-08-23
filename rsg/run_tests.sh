b7d2acb338d154a8d7e48000b2681679999696fe
Stanley Hon <stanleyhon348@gmail.com>
2014-08-23 13:46:29 +1000

    Added a testing script

---
+++ rsg/run_tests.sh
@@ -0,0 +1,14 @@
+#!/bin/sh 
+FILES=grammars/*
+for f in $FILES
+do
+    echo "Running $f..."
+    ./rsg "$f" > myOutput.txt
+    ./rsg_ref "$f" > refOutput.txt
+    if diff -q myOutput.txt refOutput.txt; then
+        echo "Passed.\n"
+    else
+        echo "FAILED!.\n"
+        diff myOutput.txt refOutput.txt
+    fi
+done
