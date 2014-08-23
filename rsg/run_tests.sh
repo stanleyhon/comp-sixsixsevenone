19faa1c6900cd58e082b0507a85b8bd23d10d3a0
Stanley Hon <stanleyhon348@gmail.com>
2014-08-23 13:53:36 +1000

    improved test runner for RSG

--- rsg/run_tests.sh
+++ rsg/run_tests.sh
@@ -6,9 +6,11 @@ do
     ./rsg "$f" > myOutput.txt
     ./rsg_ref "$f" > refOutput.txt
     if diff -q myOutput.txt refOutput.txt; then
-        echo "Passed.\n"
+        echo "Passed."
     else
-        echo "FAILED!.\n"
+        echo "FAILED!."
         diff myOutput.txt refOutput.txt
     fi
+    rm myOutput.txt
+    rm refOutput.txt
 done
