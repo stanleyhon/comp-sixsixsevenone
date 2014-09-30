bbef2938062a814dd82a41d0b164cd1b726eea10
Stanley Hon <stanleyhon348@gmail.com>
2014-09-30 17:06:14 +1000

    Untested but mostly complete Record class

--- db/record.h
+++ db/record.h
@@ -4,6 +4,8 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <map>
+#include <cassert>
 using namespace std;
 
 #include "utility.h"
@@ -20,18 +22,39 @@ enum DBSelectOperation { All, Clear, Add, Remove, Refine };
 enum DBScope { AllRecords, SelectedRecords};
 enum DBQueryOperator { Equal, NotEqual, LessThan, GreaterThan };
 
-
-// Need to add declarations for operator<< and operator >> here
-
-
 template <class value>
 class Record {
- 
+
+  // Keep data as a map of 
+  // strings 
+  // TO 
+  // std::vector <value>
+  // This is so that we have fast access to a particular attribute
+  // and all its values
+  
+  std::map <std::string, std::vector <value>> data_;
+  bool selected_; // keep track of our selected status
+
   friend ostream& operator<<<value>(ostream& out, const Record<value>& r);
   friend istream& operator>><value>(istream& in, Record<value>& r);
  
-  
+  public:
+    Record (); // empty constructor
+    ~Record (); // Destructor
+
+    bool matchesQuery(const string& attr, DBQueryOperator op, 
+            const value& want) const;
+
+    // inlined simple selected functions
+    bool isSelected () const { return selected_; };
+    void setSelected (bool val) { selected_ = val; };
+
 };
+
+template <class value>
+ostream& operator<<(ostream& out, const Record<value>& r);
+template <class value>
+ostream& operator>>(istream& in, Record<value>& r);
 
 #include "record.tem"
 
