6975b07fdfabf22672522b2acbd8212402a4f9c1
Stanley Hon <stanleyhon348@gmail.com>
2014-10-04 16:35:34 +1000

    Work in progress

--- db/database.h
+++ db/database.h
@@ -1,8 +1,45 @@
 #ifndef DATABASE_H
 #define DATABASE_H
 
-// Your database class definition goes here
+#include "record.h"
 
+// Your database class definition goes here
+template <class value>
+class Database {
+
+    // Using vector, so that selection operations
+    // occur fast regardless of how many things selected,
+    // we simply keep track of which indexes are "selected"
+    // and run selected operations on those.
+
+    // vectors offer fast random access where as list doesn't.
+    std::vector<Record<value>> records_;
+    int numRecords_;
+    int numSelected_;
+
+    // this linked list stores indexes who are "selected".
+    std::list<unsigned int> selected_; 
+
+    public:
+        Database (); // empty constructor
+        ~Database (); // Destructor
+
+        // inlined simple functions
+        int numRecords () const { return numRecords_; };
+        int numSelected () const { return numSelected_; };
+
+        void write (ostream& out, DBScope scope) const;
+
+        void read (istream& in);
+
+        void deleteRecords (DBScope scope);
+
+        void selectAll ();
+        void deselectAll ();
+
+        void select (DBSelectOperation selOp, const string& attr, 
+                DBQueryOperator op, const Value& val);
+};
 
 #include "database.tem"
 
