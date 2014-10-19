a59d76bb1aff9e1563846726d5eed704754faed7
Stanley Hon <stanleyhon348@gmail.com>
2014-10-19 16:09:16 +1100

    added a simpler test to test compilation

---
+++ btree/test00.cpp
@@ -0,0 +1,36 @@
+/**
+ * THE CODE HEREIN IS PROVIDED AS IS, WITH NO EXPRESS, OR IMPLIED, WARRANTIES
+ * OR SUITABILITY FOR PURPOSE! This means it is up to you to figure out how
+ * it works, fix it if something is broken, and adapt it as appropriate for
+ * your needs.
+ *
+ * It is supplied as a starting point for your testing. Of course, it is 
+ * inadequate as your only test and you will be in trouble if you
+ * do not write lots and lots of additional tests of your own!
+ **/
+
+#include <algorithm>
+#include <cstddef>
+#include <cstdlib>
+#include <ctime>
+#include <fstream>
+#include <iostream>
+#include <set>
+#include <string>
+
+#include "btree.h"
+
+/**
+ * Codes for testing various bits and pieces. Most of the code is commented out
+ * you should uncomment it as appropriate.
+ **/
+int main(void) {
+
+  // insert lots of random numbers and compare with a known correct container
+  btree<long> testContainer(99);
+
+  return 0;
+}
+
+
+
